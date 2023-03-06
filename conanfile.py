45c8766a2038d2f16a5e2ee3b017c9ff150363a0
MaliaLabor <malia.labor@hotmail.com>
2023-03-05 20:37:13 -0700

    :white_check_mark: Add unit tests to package build
    :bug: Updated return type for i2c_minimum_speed::transaction
    :construction_worker: Migrate to new ci repo

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,6 @@
 from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.files import copy
-from conan.tools.layout import basic_layout
 from conan.tools.build import check_min_cppstd
 from conan.errors import ConanInvalidConfiguration
 import os
@@ -11,18 +11,16 @@ required_conan_version = ">=1.50.0"
 
 class LibHALConan(ConanFile):
     name = "libhal-soft"
-    version = "0.3.2"
+    version = "1.0.0"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://libhal.github.io/libhal"
     description = ("Library for generic soft drivers officially supported by libhal")
     topics = ("soft drivers")
-    settings = "compiler"
-    exports_sources = "include/*"
+    settings = "compiler", "build_type", "os", "arch"
+    exports_sources = "include/*", "tests/*", "LICENSE"
+    generators = "CMakeToolchain", "CMakeDeps"
     no_copy_source = True
-
-    def package_id(self):
-        self.info.clear()
 
     @property
     def _min_cppstd(self):
@@ -54,12 +52,25 @@ class LibHALConan(ConanFile):
             raise ConanInvalidConfiguration(
                 f"{self.name} {self.version} requires C++{self._min_cppstd}, which your compiler ({compiler}-{version}) does not support")
 
-    def layout(self):
-        basic_layout(self)
-
     def requirements(self):
-        self.requires("libhal/0.3.2@")
-        self.requires("libhal-util/0.3.3@")
+        self.requires("libhal/[^1.0.0]")
+        self.requires("libhal-util/[^1.0.0]")
+        self.test_requires("libhal-mock/[^1.0.0]")
+        self.test_requires("boost-ext-ut/1.1.9")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        if not self.conf.get("tools.build:skip_test", default=False):
+            cmake = CMake(self)
+            if self.settings.os == "Windows":
+                cmake.configure(build_script_folder="tests")
+            else:
+                cmake.configure(build_script_folder="tests",
+                                variables={"ENABLE_ASAN": True})
+            cmake.build()
+            self.run(os.path.join(self.cpp.build.bindir, "unit_test"))
 
     def package(self):
         copy(self, "LICENSE", dst=os.path.join(
@@ -74,3 +85,7 @@ class LibHALConan(ConanFile):
         self.cpp_info.frameworkdirs = []
         self.cpp_info.libdirs = []
         self.cpp_info.resdirs = []
+        self.cpp_info.set_property("cmake_target_name", "libhal::soft")
+
+    def package_id(self):
+        self.info.clear()

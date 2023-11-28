645645105840a6ba7f00f0a9bca757d864bdc401
Khalil Estell <kammce@google.com>
2023-11-27 18:27:20 -0800

    :arrow_up: upgrade libhal post CCI update

--- conanfile.py
+++ conanfile.py
@@ -59,8 +59,8 @@ class libhal_soft_conan(ConanFile):
         self.test_requires("boost-ext-ut/1.1.9")
 
     def requirements(self):
-        self.requires("libhal/[^2.0.0]")
-        self.requires("libhal-util/[^3.0.0]")
+        self.requires("libhal/[^2.0.3]", transitive_headers=True)
+        self.requires("libhal-util/[^3.0.1]")
 
     def layout(self):
         cmake_layout(self)
